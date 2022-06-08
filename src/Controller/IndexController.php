71af7a65008111f2335847fc49201227f4b22d1a
TeddyH08 <teddy.hemonet@gmail.com>
2022-06-08 16:12:43 +0200

    liaison bdd + dynamique index

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -1,16 +1,32 @@
 <?php
+
     namespace App\Controller;
 
+    use App\Repository\ProjetsRepository;
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
     use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\Routing\Annotation\Route;
 
     class IndexController extends AbstractController {
+
+        /**
+         * @var ProjetsRepository
+         */
+        private $repository;
+
+        public function __construct(ProjetsRepository $repository){
+            $this->repository = $repository;
+        }
+
         /**
          * @Route("/index", name="index")
          * @return Response
          */
         public function index(): Response {
-            return $this->render('pages/index.html.twig');
+            $projets = $this->repository->findAll();
+
+            return $this->render('pages/index.html.twig', [
+                "projets" => $projets
+            ]);
         }
     }
---
+++ src/Entity/Projets.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProjetsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProjetsRepository::class)]
+class Projets
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $image;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $github;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $link;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getGithub(): ?string
+    {
+        return $this->github;
+    }
+
+    public function setGithub(string $github): self
+    {
+        $this->github = $github;
+
+        return $this;
+    }
+
+    public function getLink(): ?string
+    {
+        return $this->link;
+    }
+
+    public function setLink(string $link): self
+    {
+        $this->link = $link;
+
+        return $this;
+    }
+}
