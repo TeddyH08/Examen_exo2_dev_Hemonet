550a558a215d6a265a9a61a02bab7cffb6b022b4
TeddyH08 <teddy.hemonet@gmail.com>
2022-06-09 08:57:11 +0200

    add projets from bdd

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -1,16 +1,32 @@
 <?php
+
     namespace App\Controller;
 
+    use App\Repository\ProjetsexamRepository;
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
     use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\Routing\Annotation\Route;
 
     class AdminController extends AbstractController {
+
+        /**
+         * @var ProjetsRepository
+         */
+        private $repository;
+
+        public function __construct(ProjetsexamRepository $repository){
+            $this->repository = $repository;
+        }
+
         /**
          * @Route("/admin", name="admin")
          * @return Response
          */
         public function index(): Response {
-            return $this->render('pages/admin.html.twig');
+            $projets = $this->repository->findAll();
+
+            return $this->render('pages/admin.html.twig', [
+                "projets" => $projets
+            ]);
         }
     }
--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,28 +2,31 @@
 
     namespace App\Controller;
 
+    use App\Repository\ProjetsexamRepository;
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
     use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\Routing\Annotation\Route;
 
     class IndexController extends AbstractController {
 
-        // /**
-        //  * @var ProjetsRepository
-        //  */
-        // private $repository;
+        /**
+         * @var ProjetsRepository
+         */
+        private $repository;
 
-        // public function __construct(ProjetsRepository $repository){
-        //     $this->repository = $repository;
-        // }
+        public function __construct(ProjetsexamRepository $repository){
+            $this->repository = $repository;
+        }
 
         /**
          * @Route("/index", name="index")
          * @return Response
          */
         public function index(): Response {
-            // $projets = $this->repository->findAll();
+            $projets = $this->repository->findAll();
 
-            return $this->render('pages/index.html.twig');
+            return $this->render('pages/index.html.twig', [
+                "projets" => $projets
+            ]);
         }
     }
