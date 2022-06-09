772f4a77e038d97188a9009956002b8c36e3905c
TeddyH08 <teddy.hemonet@gmail.com>
2022-06-09 14:36:56 +0200

    ajout user bdd

---
+++ src/Controller/AdduserController.php
@@ -0,0 +1,51 @@
+<?php
+
+    namespace App\Controller;
+
+    use App\Entity\Admin;
+    use App\Form\AddUserType;
+    use Doctrine\ORM\EntityManagerInterface;
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\HttpFoundation\Request;
+    use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+    use Symfony\Component\Routing\Annotation\Route;
+
+    class AdduserController extends AbstractController {
+        /**
+         * @Route("/add_user", name="add_user")
+         * @return Response
+         */
+        public function index(
+            Request $request,
+            EntityManagerInterface $manager,
+            UserPasswordHasherInterface $passwordHasher
+            ): Response 
+        {
+            $users = new Admin();
+            $form = $this->createForm(AddUserType::class, $users);
+
+            $form->handleRequest($request);
+            if($form->isSubmitted() && $form->isValid()) {
+                $plaintextPassword = $users->getPassword();
+                $hashedPassword = $passwordHasher->hashPassword(
+                    $users,
+                    $plaintextPassword
+                );
+                $users->setPassword($hashedPassword);
+
+                $manager->persist($users);
+                $manager->flush();
+
+                $this->addFlash(
+                    'success',
+                    'L\'utilisateur a bien été inscrit.'
+                );
+
+                return $this->redirectToRoute('admin');
+            }
+            return $this->render('pages/add/adduser.html.twig', [
+                "form" => $form->createView()
+            ]);
+        }
+    }
---
+++ src/Form/AddUserType.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Admin;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
+class AddUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class, [
+                'attr' => [
+                    'class' => 'form_content_input'
+                ],
+                'label' => 'Nom',
+                'label_attr' => [
+                    'class' => 'form_content_label'
+                ]
+            ])
+            ->add('prenom', TextType::class, [
+                'attr' => [
+                    'class' => 'form_content_input'
+                ],
+                'label' => 'Prénom',
+                'label_attr' => [
+                    'class' => 'form_content_label'
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'attr' => [
+                    'class' => 'form_content_input'
+                ],
+                'label' => 'Addresse mail',
+                'label_attr' => [
+                    'class' => 'form_content_label'
+                ]
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => [
+                    'label' => "Mot de passe",
+                ],
+                'second_options' => [
+                    'label' => "Confirmation du mot de passe"
+                ],
+                'invalid_message' => "Les mots de passe ne  correspondent pas"
+            ])
+            ->add('photo', TextType::class, [
+                'label' => "Image de profil",
+                'data' => "user.png"
+            ])
+            ->add('submit', SubmitType::class, [
+                'attr' => [
+                    'class' => 'form_content_submit'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Admin::class,
+        ]);
+    }
+}
